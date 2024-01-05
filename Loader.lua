66f4c6ac3417be0a7de0a27141b766c98573ec4b
doxbinlol <155278379+doxbinlol@users.noreply.github.com>
2024-01-05 10:45:12 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,3 @@
-print("source")
+-- [[ Opponent.cc Source Code Brought To You By listofmyregrets AHAHAHAHAHAHAHAHAHHAHAHAHAHAHA -- ]]
+-- [[ Credits to colloper for the gun fov + credits to whoever made some parts of the src code -- ]]
+-- [[ DOXBIN.LOL VAULT ON TOP AHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA -- ]]
